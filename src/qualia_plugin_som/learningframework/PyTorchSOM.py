73be8edc4f1aaefeef0314699bafe26b4339fbae
piernov <piernov@piernov.org>
2024-06-10 19:15:08 +0200

    PyTorchSOM: fix for qualia_core

--- src/qualia_plugin_som/learningframework/PyTorchSOM.py
+++ src/qualia_plugin_som/learningframework/PyTorchSOM.py
@@ -1,27 +1,38 @@
-import torchmetrics
-from qualia_core.learningframework import PyTorch
-from qualia_plugin_som.learningmodel.pytorch.layers import SOMLabelling
-from pytorch_lightning import Trainer
+from __future__ import annotations
+
+import logging
+import os
+import sys
+
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from qualia_core.learningframework import PyTorch
+from qualia_core.typing import TYPE_CHECKING
 from torch.utils.data import DataLoader
 
+if TYPE_CHECKING:
+    from pytorch_lightning import Callback  # noqa: TCH002
+    from pytorch_lightning.trainer.connectors.accelerator_connector import _PRECISION_INPUT  # noqa: TCH002
+    from qualia_core.dataaugmentation.DataAugmentation import DataAugmentation  # noqa: TCH002
+    from qualia_core.datamodel.RawDataModel import RawData  # noqa: TCH002
+    from qualia_core.experimenttracking.ExperimentTracking import ExperimentTracking  # noqa: TCH002
+    from qualia_core.typing import OptimizerConfigDict
+    from torch import nn  # noqa: TCH002
+    import torch
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
+
+logger = logging.getLogger(__name__)
 
 class PyTorchSOM(PyTorch):
     import qualia_plugin_som.learningmodel.pytorch as learningmodels
     learningmodels.__dict__.update(PyTorch.learningmodels.__dict__) # Merge core models back. Warning: module name changes too!
 
     class TrainerModuleSOM(PyTorch.TrainerModule):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-            metrics = torchmetrics.MetricCollection({
-                'mse': torchmetrics.MeanSquaredError(),
-            })
-            self.train_metrics = metrics.clone(prefix='train')
-            self.val_metrics = metrics.clone(prefix='val')
-            self.test_metrics = metrics.clone(prefix='test')
-
-        def training_step(self, batch, batch_nb):
+        @override
+        def training_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_nb: int) -> torch.Tensor:
             x, y = batch
             if len(x.shape) > 2:
                 x = self.model.flatten(x)
@@ -29,25 +40,25 @@ class PyTorchSOM(PyTorch):
             self.train_metrics(bmu_vectors, x)
             self.log_dict(self.train_metrics, prog_bar=True)
 
-        def validation_step(self, batch, batch_nb):
+        @override
+        def validation_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_nb: int) -> None:
             x, y = batch
             if len(x.shape) > 2:
                 x = self.model.flatten(x)
             bmu_indices, bmu_vectors = self.model.som(x)
             self.val_metrics(bmu_vectors, x)
             self.log_dict(self.val_metrics, prog_bar=True)
-        
-        def on_train_epoch_end(self):
-            pass
 
     class TrainerModuleSOMLabelling(PyTorch.TrainerModule):
-        def training_step(self, batch, batch_nb):
+        @override
+        def training_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_nb: int) -> torch.Tensor:
             x, y = batch
             logits = self.model(x, y)
             self.train_metrics(logits, y)
             self.log_dict(self.train_metrics, prog_bar=True)
 
-        def validation_step(self, batch, batch_nb):
+        @override
+        def validation_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_nb: int) -> None:
             x, y = batch
             logits = self.model(x, y)
             self.val_metrics(logits, y)
@@ -56,7 +67,7 @@ class PyTorchSOM(PyTorch):
         def configure_callbacks(self):
             return [self.model]
 
-    def plot_neurons(self, model, trainset):
+    def plot_neurons(self, model: nn.Module, trainset: RawData):
         import matplotlib.pyplot as plt
 
         map_wth = model.som.out_features[0]
@@ -72,7 +83,7 @@ class PyTorchSOM(PyTorch):
                 #sub.set_title(model.som_labelling.labels[x][y].argmax())
                 #data = model.som.neurons[x][y]
                 #data = data.reshape(data.shape[0] * data.shape[1])
-                sub.set_title(model.som_labelling.labels[x][y].argmax())
+                _ = sub.set_title(model.som_labelling.labels[x][y].argmax())
                 data = model.som.neurons[x * map_hgt + y]
                 data = data.reshape((model.input_shape[1], model.input_shape[0]))
                 #data = data.swapaxes(0, 1)
@@ -84,36 +95,102 @@ class PyTorchSOM(PyTorch):
 
         plt.show()
 
-    def train(self, model, trainset, validationset, epochs, batch_size, optimizer, dataaugmentations=None, experimenttracking=None, name=None):
-        print(f'Training SOM, {epochs=}')
+    @override
+    def train(self,
+              model: nn.Module,
+              trainset: RawData | None,
+              validationset: RawData | None,
+              epochs: int,
+              batch_size: int,
+              optimizer: OptimizerConfigDict | None,
+              dataaugmentations: list[DataAugmentation] | None = None,
+              experimenttracking: ExperimentTracking | None = None,
+              name: str | None = None,
+              precision: _PRECISION_INPUT | None = None) -> nn.Module:
+        logger.info('Training SOM')
+
+        ## Copied from qualia_core.learningframework.PyTorch, need to refactor one day…
+        from pytorch_lightning import Trainer, seed_everything
+        from pytorch_lightning.callbacks import ModelCheckpoint, TQDMProgressBar
+
+        # PyTorch-Lightning >= 1.3.0 resets seed before training
+        # increment seed between trainings to get different values between experiments
+        seed = os.environ.get('PL_GLOBAL_SEED', None)
+        if seed is None:
+            logger.warning('PyTorch not seeded')
+        else:
+            _ = seed_everything((int(seed) * 100) % 4294967295)
+
+        checkpoint_callback = ModelCheckpoint(dirpath=f'out/checkpoints/{name}',
+                                              save_top_k=2,
+                                              monitor=self._checkpoint_metric['name'],
+                                              mode=self._checkpoint_metric['mode'])
+        callbacks: list[Callback] = [checkpoint_callback]
+        if self._enable_progress_bar:
+            callbacks.append(TQDMProgressBar(refresh_rate=self._progress_bar_refresh_rate))
+
+        experimenttracking_init = experimenttracking.initializer if experimenttracking is not None else None
+
+        ### End of copy
+
+        # Stop training when not converging or diverging
+        early_stopping_callback = EarlyStopping(monitor='trainmse',
+                                                mode='min',
+                                                min_delta=0.0,
+                                                patience=9999,
+                                                check_finite=True,
+                                                strict=True)
         # Train SOM
-        early_stop_callback = EarlyStopping(monitor="trainmse", mode='min', min_delta=0.0, patience=9999, check_finite=True, strict=True)
         trainer_som = Trainer(max_epochs=epochs,
                               accelerator=self.accelerator,
                               devices=self.devices,
+                              precision=self.precision if precision is None else precision,
                               deterministic=True,
                               logger=self.logger(experimenttracking, name=name),
                               enable_progress_bar=self._enable_progress_bar,
-                              callbacks=[early_stop_callback])
-        trainer_module_som = self.TrainerModuleSOM(model=model, max_epochs=epochs, optimizer=None, dataaugmentations=dataaugmentations, num_classes=trainset.y.shape[-1])
+                              callbacks=[*callbacks, early_stopping_callback])
+        trainer_module_som = self.TrainerModuleSOM(model=model,
+                                                   max_epochs=epochs,
+                                                   optimizer=None,
+                                                   dataaugmentations=dataaugmentations,
+                                                   num_outputs=trainset.y.shape[-1],
+                                                   experimenttracking_init=experimenttracking_init,
+                                                   loss=None,
+                                                   metrics=['mse'])
         trainer_som.fit(trainer_module_som,
-                            DataLoader(self.DatasetFromArray(trainset), batch_size=batch_size, shuffle=True),
-                            DataLoader(self.DatasetFromArray(validationset), batch_size=batch_size) if len(validationset.x) > 0 else None
+                        DataLoader(self.DatasetFromArray(trainset),
+                                   batch_size=batch_size,
+                                   shuffle=True),
+                        DataLoader(self.DatasetFromArray(validationset),
+                                   batch_size=batch_size) if validationset is not None else None,
                         )
 
 
-        print('Labelling SOM')
+        logger.info('Labelling SOM')
         # Label SOM
         trainer_som_labelling = Trainer(max_epochs=1,
                                         accelerator=self.accelerator,
                                         devices=self.devices,
+                                        precision=self.precision if precision is None else precision,
                                         deterministic=True,
                                         logger=self.logger(experimenttracking, name=name),
                                         enable_progress_bar=self._enable_progress_bar)
-        trainer_module_som_labelling = self.TrainerModuleSOMLabelling(model=model.som_labelling, max_epochs=1, optimizer=None, dataaugmentations=dataaugmentations, num_classes=trainset.y.shape[-1])
+        trainer_module_som_labelling = self.TrainerModuleSOMLabelling(model=model.som_labelling,
+                                                                      max_epochs=1,
+                                                                      optimizer=None,
+                                                                      dataaugmentations=dataaugmentations,
+                                                                      num_outputs=trainset.y.shape[-1],
+                                                                      experimenttracking_init=experimenttracking_init,
+                                                                      loss=None,
+                                                                      metrics=['prec', 'rec', 'f1', 'acc', 'avgclsacc'])
         trainer_som_labelling.fit(trainer_module_som_labelling,
-                            DataLoader(self.DatasetFromArray(trainset), batch_size=32, shuffle=True),
-                            DataLoader(self.DatasetFromArray(validationset), batch_size=32) if len(validationset.x) > 0 else None
-                        )
+                                  DataLoader(self.DatasetFromArray(trainset),
+                                             batch_size=32,
+                                             shuffle=True),
+                                  DataLoader(self.DatasetFromArray(validationset),
+                                             batch_size=32) if validationset is not None else None,
+                                  )
         #self.plot_neurons(model, trainset)
-        print('SOM training and labelling finished')
+        logger.info('SOM training and labelling finished')
+
+        return model
